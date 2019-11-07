6da1cb8b0e26c0574be1f6b0f53e387caa048cd2
troussej <trousset.joel@gmail.com>
2019-11-07 23:25:21 +0100

    fix: static

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist/sliver-builder";
 app.use(express.static(distDir));
+
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
