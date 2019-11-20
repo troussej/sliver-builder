89f004843009d1c09a17cf3ce604cc972a75af88
Joël TROUSSET <trousset.joel@gmail.com>
2019-11-20 15:41:29 +0100

    fix: logs

--- packages/server/src/util/logger.ts
+++ packages/server/src/util/logger.ts
@@ -1,32 +1,38 @@
 import winston from "winston";
-
+import _ from 'lodash';
 export class LogProvider {
 
-    logger: any;
+  logger: any;
 
-    public static getLogger(): winston.Logger {
-        const options: winston.LoggerOptions = {
-            format: winston.format.combine(
-                winston.format.splat(),
-                winston.format.colorize(),
-                winston.format.simple()
-            ),
-            transports: [
-                new winston.transports.Console({
-                    level: process.env.NODE_ENV === "production" ? "error" : "debug"
-                })
-                // new winston.transports.File({ filename: "debug.log", level: "debug" })
-            ]
-        };
+  public static getLogger(): winston.Logger {
+    let level = process.env["LOG_LEVEL"]
 
-        const logger: winston.Logger = winston.createLogger(options);
-
-        if (process.env.NODE_ENV !== "production") {
-            logger.debug("Logging initialized at debug level");
-        }
-
-        return logger;
+    if (_.isNil(level)) {
+      level = process.env.NODE_ENV === "production" ? "error" : "debug";
     }
+
+    const options: winston.LoggerOptions = {
+      format: winston.format.combine(
+        winston.format.splat(),
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+      transports: [
+        new winston.transports.Console({
+          level
+        })
+        // new winston.transports.File({ filename: "debug.log", level: "debug" })
+      ]
+    };
+
+    const logger: winston.Logger = winston.createLogger(options);
+
+    if (process.env.NODE_ENV !== "production") {
+      logger.debug("Logging initialized at %s level", level);
+    }
+
+    return logger;
+  }
 
 
 }
