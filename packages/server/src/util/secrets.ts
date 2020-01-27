497eb79e4b3122ff3cf3b6a9c9e17328e4e4657e
Trousset <joel.trousset@cgi.com>
2020-01-27 17:14:05 +0100

    feat: dropping mongodb check since not used

--- packages/server/src/util/secrets.ts
+++ packages/server/src/util/secrets.ts
@@ -23,11 +23,11 @@ export const MONGODB_URI = prod ? process.env.MONGODB_URI : process.env.MONGODB_
 //     process.exit(1);
 // }
 
-if (!MONGODB_URI) {
-  if (prod) {
-      logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
-    } else {
-      logger.error('No mongo connection string. Set MONGODB_URI_LOCAL environment variable.');
-    }
-  process.exit(1);
-}
+// if (!MONGODB_URI) {
+//   if (prod) {
+//       logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
+//     } else {
+//       logger.error('No mongo connection string. Set MONGODB_URI_LOCAL environment variable.');
+//     }
+//   process.exit(1);
+// }
